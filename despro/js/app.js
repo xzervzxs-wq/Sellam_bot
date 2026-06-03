d87f72b6353fd8ac1fa606bc0b588f308de4f2f4
xzervzxs-wq <xzervzxs@gmail.com>
2026-06-04 02:10:44 +0300

    Force canvas dimensions to A4 portrait layout

--- despro/js/app.js
+++ despro/js/app.js
@@ -1994,13 +1994,11 @@
 
                 let finalCanvasW, finalCanvasH, cols, rows;
 
-                if (landscapeCount > portraitCount) {
-                    finalCanvasW = A4_HEIGHT; finalCanvasH = A4_WIDTH;
-                    cols = landscapeCols; rows = landscapeRows;
-                } else {
-                    finalCanvasW = A4_WIDTH; finalCanvasH = A4_HEIGHT;
-                    cols = portraitCols; rows = portraitRows;
-                }
+                // دائماً استخدم Portrait (الطول)
+finalCanvasW = A4_WIDTH;
+finalCanvasH = A4_HEIGHT;
+cols = portraitCols;
+rows = portraitRows;
 
                 currentA4Layout = {
                     canvasW: finalCanvasW, canvasH: finalCanvasH,
